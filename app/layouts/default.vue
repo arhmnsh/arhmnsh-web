093cd4c5519286b2f28bbcc3d22d58d7f07574a4
arhmnsh <a.rahmanshah@gmail.com>
2026-01-07 12:59:45 +0530

    fix: explicitly set ml-0 on mobile for content alignment

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,7 +11,7 @@
   <div class="flex min-h-screen bg-background font-sans text-foreground">
     <NuxtLoadingIndicator color="#888" :height="2" />
     <Sidebar />
-    <div class="flex flex-1 flex-col lg:ml-64 min-w-0 w-full pb-14 lg:pb-0">
+    <div class="flex flex-1 flex-col ml-0 lg:ml-64 min-w-0 w-full pb-14 lg:pb-0">
       <MobileNav />
       <div class="flex-1 min-w-0 w-full">
         <slot />
