37a11ae7051187c57a69e77a012229aeaf24b162
arhmnsh <a.rahmanshah@gmail.com>
2026-01-13 14:10:05 +0530

    fix(home): force gpu acceleration for smoother image transition

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -70,7 +70,7 @@ const { data: latestArticles } = await useAsyncData('latest-articles', () =>
         <!-- Right: Image -->
         <div class="order-1 lg:order-2">
           <div class="relative w-full aspect-square rounded-xl overflow-hidden bg-muted/20">
-            <img src="/images/me.jpg" alt="AbdurRahaman Shah" class="w-full h-full object-cover grayscale transform transition-all duration-300 ease-out hover:scale-[1.02] hover:grayscale-0" />
+            <img src="/images/me.jpg" alt="AbdurRahaman Shah" class="w-full h-full object-cover grayscale transform-gpu transition-all duration-500 ease-out hover:scale-[1.02] hover:grayscale-0 will-change-transform" />
           </div>
         </div>
       </section>
